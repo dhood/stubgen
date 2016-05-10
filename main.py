f6a882bb2dc4c043bed9eb38af647f902622d607
Viraj Sinha <virajosinha@gmail.com>
2016-05-10 15:08:04 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import ast
+
+FILENAME = "./test/__init__.py"
+
+
+def grabThingsBetweenColons(string):
+    if (string[0] is "@"):
+        return
+    elif (string[0] is ":"):
+        # we're doxygen
+        return
+
+def main():
+    with open(FILENAME) as fd:
+        file_contents = fd.read()
+        module = ast.parse(file_contents)
+
+        print('loaded file')
+
+        function_definitions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
+
+        for f in function_definitions:
+            # print('---')
+            # print(f.name)
+            # print('---')
+            # print(ast.get_docstring(f))
+            docstring = ast.get_docstring(f)
+            params = {}
+            returntype = ""
+            for line in docstring.splitlines():
+                # if ":param" in line:
+                    # param = line.split()[2]
+                    # params[param] =  "Any"
+                if ":type" in line:
+
+                    # this line is a type
+                    tokens = line.split()
+                    param = tokens[1][:-1]
+                    ty = tokens[2]
+                    # print ("param is " + param + " with type " + ty)
+                    params[param] = ty
+            string = "def {0}({1})".format(
+                   f.name, ", ".join(
+                       ["%s: %s" % (param, params[param]) for param in params]))
+            '''
+            string = "def " + f.name + "("
+            for param in params:
+                string += "" + param + ": " + params[param] + ", "
+            string += "):"
+            '''
+            print string
+
+
+if __name__ == '__main__': 
+    main()
---
+++ test/docstrings.py
@@ -0,0 +1,36 @@
+def foo(alpha, beta, gamma):
+    """
+    :param alpha: apples
+    :type alpha: int
+
+    :param beta: bananas
+
+    :type gamma: str
+    :param gamma: grapes
+
+    :returns: some fruits
+    :rtype: int
+    """
+    return 1
+
+def bar(beta):
+    """
+    :param beta: bananas
+    :returns: some fruits
+    """
+    return []
+
+def my_method(x, y):
+    """
+    my_method description
+
+    @type x: int
+    @param x: an integer
+
+    @type y: int|string
+    @param y: an integer or string
+
+    @rtype: string
+    @return: returns a sentence with variables
+    """
+    return ""
