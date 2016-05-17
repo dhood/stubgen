65b5a9df780069a3a39ad17cf9bd4ea6d93f2563
Deanna Hood <deanna.m.hood@gmail.com>
2016-05-16 22:44:16 -0700

    Generate the stubs file

--- examples/sphinx_parser.py
+++ examples/sphinx_parser.py
@@ -12,13 +12,41 @@ outdir = os.path.join(srcdir, 'doc')
 doctreedir = os.path.join(outdir, '.doctrees')
 buildername = 'dummy'
 
+filename = 'output.pyi'
+
 def process_docstring(app, what, name, obj, options, lines):
     if lines:
-        print name
-        print lines
+        docfields = parse_docstring(':', ':', lines)
+        print('----')
+        params = {}
+        returnType = None
+        for field in docfields:
+            fieldType = field['fieldType']
+            if fieldType == 'param':
+                fieldArg = field['fieldArg']
+                if 'fieldVal' in field:
+                    params[fieldArg] = field['fieldVal']
+                else:
+                    params[fieldArg] = 'Any'
+        for field in docfields:
+            fieldType = field['fieldType']
+            if fieldType == 'type':
+                fieldArg = field['fieldArg']
+                if fieldArg in params:
+                    params[fieldArg] = field['fieldText']
+        shortName = name.split('.')[-1]
+        string = 'def {0}({1}) -> {2}: ...\n\n'.format(
+            shortName,
+            ', '.join(
+                ['%s: %s' % (param, params[param]) for param in params]),
+            returnType if returnType else 'Any')
+        with open(filename, 'a') as f:
+            f.write(string)
 
 if __name__ == '__main__':
+    with open(filename, 'w') as f:
+        f.write('from typing import Any\n\n')
+
     app = Sphinx(srcdir, confdir, outdir, doctreedir, buildername, freshenv=True)
     app.connect('autodoc-process-docstring', process_docstring)
     app.build()
-
